d1fd6171b1534e869966659db78d193bfff3687d
vibhushit <vibhushit10@gmail.com>
2024-10-27 23:19:07 +0530

    add: inherent implementaion

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,25 @@ struct Deck {
     cards : Vec<String>
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamongs", "Clubs"];
-    // let values = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let values = ["A", "1", "2"];
-    let mut cards = Vec::new();
-
-
-    for suit in suits {
-        for value in values {
-            let card =  format!("{} of {}", value, suit);
-            cards.push(card);
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamongs", "Clubs"];
+        // let values = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+        let values = ["A", "1", "2"];
+        let mut cards = Vec::new();
+    
+        for suit in suits {
+            for value in values {
+                let card =  format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        Deck { cards }
     }
+}
 
-
-    let cards = Deck {cards };
-
+fn main() {
+    let cards = Deck::new();
     println!("Deck is {:#?}", cards);
 }
