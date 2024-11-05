a8da3fd07197857333e6791777a57204f0611f7b
vibhushit <vibhushit10@gmail.com>
2024-11-05 23:56:32 +0530

    add: deal function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use rand::{thread_rng, seq::SliceRandom}
+use rand::{seq::SliceRandom, thread_rng};
 
 #[derive(Debug)]
 struct Deck {
-    cards : Vec<String>
+    cards: Vec<String>,
 }
 
 impl Deck {
@@ -11,19 +11,35 @@ impl Deck {
         // let values = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
         let values = ["A", "1", "2"];
         let mut cards = Vec::new();
-    
+
         for suit in suits {
             for value in values {
-                let card =  format!("{} of {}", value, suit);
+                let card = format!("{} of {}", value, suit);
                 cards.push(card);
             }
         }
 
         Deck { cards }
     }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 
 fn main() {
-    let cards = Deck::new();
+    let mut cards = Deck::new();
+
+    cards.shuffle();
+
+    //need to add some error handling !!!
+    let hand = cards.deal(3);
+
+    println!("Hand is {:#?}", hand);
     println!("Deck is {:#?}", cards);
 }
